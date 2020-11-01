50fb91185b6012905a0580dea55e58a6173673e4
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2020-11-01 07:23:17 -0700

    Fix a comment in SetStringParameter

--- src/HeifEncoder.cs
+++ src/HeifEncoder.cs
@@ -376,7 +376,7 @@ namespace LibHeifSharp
 
             if (coerceParameterType)
             {
-                // Some encoders expect the method that is called will match the parameter type.
+                // Some encoders expect the method that is called to match the parameter type.
                 // Attempting to set a Boolean or Integer parameter as a string will cause an invalid parameter error.
                 if (this.encoderParameterTypes.Value.TryGetValue(name, out var type))
                 {
